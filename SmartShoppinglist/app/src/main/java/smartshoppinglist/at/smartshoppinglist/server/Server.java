c1c139da2d1244d4d1abb04b31ed10289f805300
Clements Wagner <clements.wagner@gmx.at>
2019-04-09 12:39:51 +0200

    Added server connectivity and login function

---
+++ SmartShoppinglist/app/src/main/java/smartshoppinglist/at/smartshoppinglist/server/Server.java
@@ -0,0 +1,67 @@
+package smartshoppinglist.at.smartshoppinglist.server;
+
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+public class Server {
+
+    private static Server instance;
+    HttpConnection http;
+
+    public static Server getInstance(){
+        if(instance == null){
+            try {
+                instance = new Server();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return instance;
+    }
+
+    private Server() throws Exception{
+        TrustManager[] trustAllCerts = new TrustManager[] {
+                new X509TrustManager() {
+                    public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                        return null;
+                    }
+
+                    public void checkClientTrusted(X509Certificate[] certs, String authType) {  }
+
+                    public void checkServerTrusted(X509Certificate[] certs, String authType) {  }
+
+                }
+        };
+
+        SSLContext sc = SSLContext.getInstance("SSL");
+        sc.init(null, trustAllCerts, new java.security.SecureRandom());
+        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+
+        // Create all-trusting host name verifier
+        HostnameVerifier allHostsValid = new HostnameVerifier() {
+            public boolean verify(String hostname, SSLSession session) {
+                return true;
+            }
+        };
+        // Install the all-trusting host verifier
+        HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+
+       http = new HttpConnection();
+    }
+
+    public boolean login(String email, String password){
+        boolean result = false;
+        try {
+            result = Boolean.parseBoolean(http.sendGet("Login?email="+email+"&password="+password));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+}
